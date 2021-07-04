d5f22829fedd0ac63ca3dde3e3b02be2358fb94a
Elena Volkova <lenochka.volkova@gmail.com>
2021-07-04 23:33:37 +0300

    fixes

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -12,12 +12,12 @@ def welcome_user():
 def ask_question():
     operand1 = randint(1, 100)
     operand2 = randint(1, 100)
-    operator_number = randint(0, 2)
-    operators = ('+', '-', '*')
-    print('Question: {} {} {}'.format(operand1, operators[operator_num], operand2))
-    if operator_number == 0:
+    operator_num = randint(0, 2)
+    oper = ('+', '-', '*')
+    print('Question: {} {} {}'.format(operand1, oper[operator_num], operand2))
+    if operator_num == 0:
         answer = operand1 + operand2
-    elif operator_number == 1:
+    elif operator_num == 1:
         answer = operand1 - operand2
     else:
         answer = operand1 * operand2
